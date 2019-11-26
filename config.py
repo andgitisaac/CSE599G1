740adbd95419048e4e4a50fdb3d83c4359831e14
hjpeng <hjpeng@uw.edu>
2019-11-26 02:18:33 -0800

    Configuration

--- config.py
+++ config.py
@@ -1,14 +1,27 @@
 import os
 
 # Enviromnet Settings
-# USE_CUDA = True
+USE_CUDA = True
 TRAIN_DIR = 'data'
+MODEL_DIR = 'model'
+SAMPLE_DIR = 'sample'
+LOG_DIR = 'log'
+
+# Network Settings
+IMG_SIZE = 64
+ZDIM = 100
+NGF = 32
+NDF = 32
+
 
 # Training Settings
-EPOCHS = 200
-BATCH_SIZE = 64
-LR = 1e-2
-SIZE = 64
+SEED = 6666
 
-WORKERS = 2
+EPOCHS = 50
+BATCH_SIZE = 64
+LR = 0.0005
+LOG_STEP = 50
+FLIP = 0.3
+
+NGPU = 1
 
